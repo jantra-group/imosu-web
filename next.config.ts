cc4e731f9faada2de82234ed6ec6b407060100d4
hildazaqya <hildazaqyaelnaz@gmail.com>
2025-03-26 15:55:37 +0700

    fix bug

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,11 @@
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
-    domains: [
-      'scontent.cdninstagram.com',
-      'scontent-iad3-1.cdninstagram.com',
-      'scontent-iad3-2.cdninstagram.com',
-      'scontent-cgk1-1.cdninstagram.com',
-      'mdtswauykfhfprscrcld.supabase.co',
+    domains: ['scontent.cdninstagram.com', 'scontent-iad3-1.cdninstagram.com', 'scontent-iad3-2.cdninstagram.com', 'scontent-cgk1-1.cdninstagram.com',
+      'mdtswauykfhfprscrcld.supabase.co'
     ],
   },
-  swcMinify: false, // Nonaktifkan minifikasi selama debugging
-  experimental: { css: false }, // Tambahkan jika errornya terkait dengan CSS
 };
 
-module.exports = nextConfig;
+export default nextConfig;
