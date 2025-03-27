1a6cdb760877b5cbe360f7e9a2fed1e14930bd61
hildazaqya <hildazaqyaelnaz@gmail.com>
2025-03-27 10:01:28 +0700

    fix bug

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  swcMinify: false, // Disable SWC minifier temporarily
+  webpack(config) {
+    config.optimization = {
+      ...config.optimization,
+      minimize: false, // Disable CSS minimizer temporarily
+    };
+    return config;
+  },
   images: {
-    domains: ['scontent.cdninstagram.com', 'scontent-iad3-1.cdninstagram.com', 'scontent-iad3-2.cdninstagram.com', 'scontent-cgk1-1.cdninstagram.com',
-      'mdtswauykfhfprscrcld.supabase.co'
+    domains: [
+      'scontent.cdninstagram.com',
+      'scontent-iad3-1.cdninstagram.com',
+      'scontent-iad3-2.cdninstagram.com',
+      'scontent-cgk1-1.cdninstagram.com',
+      'mdtswauykfhfprscrcld.supabase.co',
     ],
   },
 };
